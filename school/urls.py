d866e5c7d3e6325a905394de7eedb16f5349682f
User1 <431410@mail.ru>
2024-09-16 20:50:04 +0900

    create urls

---
+++ school/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from school.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonUpdateAPIView, LessonRetrieveAPIView, LessonDestroyAPIView
+from school.apps import SchoolConfig
+
+
+app_name = SchoolConfig.name
+
+router = SimpleRouter()
+router.register('course', CourseViewSet)
+
+urlpatterns = [
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lessons_create'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lessons_detail'),
+    path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lessons_update'),
+    path('lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lessons_delete'),
+    path('lessons/', LessonListAPIView.as_view(), name='lessons_list'),
+] + router.urls
