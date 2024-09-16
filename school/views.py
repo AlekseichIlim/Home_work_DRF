ce129ce25d32a348378e064ab622ef6159d7f04b
User1 <431410@mail.ru>
2024-09-16 20:50:39 +0900

    create Views

--- school/views.py
+++ school/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from school.models import Course, Lesson
+from school.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    serializer_class = LessonSerializer
