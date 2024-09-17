fe0e8b0698649ee414323d53659035fc4420b8a9
User1 <431410@mail.ru>
2024-09-17 22:07:58 +0900

    create model Payments

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from school.models import Course, Lesson
+
 NULLABLE = {'blank': True, 'null': True}
 
 
@@ -21,3 +23,25 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
+
+WAY_CHOICES = [
+        (0, 'наличные'),
+        (1, 'перевод'),
+    ]
+
+
+class Payments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    date_pay = models.DateField(auto_now_add=True, verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='Курс', **NULLABLE, related_name='payments')
+    lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='Урок', **NULLABLE, related_name='payments')
+    amount = models.IntegerField(default=0, verbose_name='сумма оплаты')
+    way = models.CharField(max_length=20, choices=WAY_CHOICES, verbose_name='способ оплаты')
+
+    def __str__(self):
+        return f'{self.user} {self.date_pay} оплатил {self.amount}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ('-date_pay', )
