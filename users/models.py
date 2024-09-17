f31d18d7ab517f304b50628476ff1882817797c6
User1 <431410@mail.ru>
2024-09-17 09:52:13 +0900

    create Views user

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Email адрес')
