625b3050ba47f84810c4de897cbd3c4c93973d90
User1 <431410@mail.ru>
2024-09-17 22:08:41 +0900

    add count lessons

--- school/serializers.py
+++ school/serializers.py
@@ -4,15 +4,19 @@ from school.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
 #         поля которые будут в ответе Postman
 
+    def get_count_lessons(self, instance):
+
+        return instance.lesson.all().count()
+
 
 class LessonSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Lesson
-        fields = ('title', 'course', )
+        fields = ('id', 'title', 'course', )
