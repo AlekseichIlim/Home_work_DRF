b2060da7c760a4293a2c1d5d733ec50b99310f7b
User1 <431410@mail.ru>
2024-09-17 09:52:28 +0900

    add url user

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('school/', include('school.urls', namespace='school')),
+    path('users/', include('users.urls', namespace='users')),
 
 ]
