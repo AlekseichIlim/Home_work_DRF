fa19849a9e66fc62fe9bc8ae0f4c8a0e55b8dc9d
User1 <431410@mail.ru>
2024-09-17 22:33:03 +0900

    add django-filter

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
