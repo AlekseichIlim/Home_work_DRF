580ee5d09c1cf84da9d07279f61c5c07799bea9f
User1 <431410@mail.ru>
2024-09-19 01:29:46 +0900

    corrected

--- school/views.py
+++ school/views.py
@@ -1,17 +1,47 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 
 from school.models import Course, Lesson
 from school.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerPermission, IsOwnerPermission
 
 
 class CourseViewSet(ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+    def perform_create(self, serializer):
+        """Назначение владельца курса"""
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
+
+    def get_permissions(self):
+        """Разрешение для НЕмодераторов"""
+        if self.action == "create":
+            self.permission_classes = (~IsModerPermission,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsOwnerPermission | IsModerPermission,)
+        elif self.action == "delete":
+            self.permission_classes = (~IsModerPermission, IsOwnerPermission,)
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = (~IsModerPermission,)
+
+    def perform_create(self, serializer):
+        """Назначение владельца урока"""
+        lesson = serializer.save()
+        lesson.owner = self.request.user
+        lesson.save()
 
 
 class LessonListAPIView(ListAPIView):
@@ -22,12 +52,16 @@ class LessonListAPIView(ListAPIView):
 class LessonRetrieveAPIView(RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = (IsOwnerPermission | IsModerPermission,)
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = (IsOwnerPermission | IsModerPermission,)
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+    permission_classes = (~IsModerPermission, IsOwnerPermission,)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,12 @@
 from rest_framework import filters
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    DestroyAPIView,
+    UpdateAPIView,
+    RetrieveAPIView,
+)
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import User, Payments
@@ -11,6 +18,34 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
 
 
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    serializer_class = UserSerializer
 
 
 class PaymentsCreateAPIView(CreateAPIView):
@@ -22,5 +57,9 @@ class PaymentsListAPIView(ListAPIView):
     serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
     filter_backends = [filters.OrderingFilter]
-    filterset_fields = ('course', 'lesson', 'way',)
-    ordering_fields = ('date_pay', 'amount')
+    filterset_fields = (
+        "course",
+        "lesson",
+        "way",
+    )
+    ordering_fields = ("date_pay", "amount")
