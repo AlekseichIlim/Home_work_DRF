3e7071dfa73b9015b7cc3411335873834bf67a6b
User1 <431410@mail.ru>
2024-09-19 01:29:50 +0900

    corrected

--- school/serializers.py
+++ school/serializers.py
@@ -6,21 +6,24 @@ from school.models import Course, Lesson
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = ('id', 'title', 'course', )
+        fields = (
+            "id",
+            "title",
+            "course",
+            "owner"
+        )
 
 
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
-    lesson = LessonSerializer(many=True)
+    lesson = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
-        fields = '__all__'
-#         поля которые будут в ответе Postman
+        fields = "__all__"
+
+    #         поля которые будут в ответе Postman
 
     def get_count_lessons(self, instance):
 
         return instance.lesson.all().count()
-
-
-
--- users/serializers.py
+++ users/serializers.py
@@ -6,12 +6,13 @@ from users.models import User, Payments
 class PaymentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payments
-        fields = '__all__'
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
-    payments = PaymentsSerializer(many=True)
+    payments = PaymentsSerializer(many=True, read_only=True)
+    # read_only - при создании не будет требовать ввод поля
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = "__all__"
