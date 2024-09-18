ca03406a4c4d2e83315e0bd43151fae2ac8b7b5b
User1 <431410@mail.ru>
2024-09-19 01:28:37 +0900

    corrected url

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('school/', include('school.urls', namespace='school')),
-    path('users/', include('users.urls', namespace='users')),
-
+    path("admin/", admin.site.urls),
+    path("school/", include("school.urls", namespace="school")),
+    path("users/", include("users.urls", namespace="users")),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,33 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from users.views import UserViewSet, PaymentsCreateAPIView, PaymentsListAPIView
+from users.views import (
+    UserViewSet,
+    PaymentsCreateAPIView,
+    PaymentsListAPIView,
+    UserListAPIView,
+    UserCreateAPIView,
+    UserRetrieveAPIView,
+    UserUpdateAPIView,
+)
+
 from users.apps import UsersConfig
 
 
 app_name = UsersConfig.name
 
-router = SimpleRouter()
-router.register('', UserViewSet)
+# router = SimpleRouter()
+# router.register('', UserViewSet)
 
 urlpatterns = [
-    path('payments/create/', PaymentsCreateAPIView.as_view(), name='payments_create'),
-    path('payments/', PaymentsListAPIView.as_view(), name='payments_list')
-] + router.urls
+    path("", UserListAPIView.as_view(), name="user_list"),
+    path("register/", UserCreateAPIView.as_view(), name="user_register"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login",),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh",),
+    path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user_detail"),
+    path("<int:pk>/update/", UserUpdateAPIView.as_view(), name="user_update"),
+    path("<int:pk>/delete/", PaymentsCreateAPIView.as_view(), name="user_delete"),
+    path("payments/create/", PaymentsCreateAPIView.as_view(), name="payments_create"),
+    path("payments/", PaymentsListAPIView.as_view(), name="payments_list"),
+]
